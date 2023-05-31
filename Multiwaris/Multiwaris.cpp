967aa0fdccd24fe30f7e51ba5e7c55a04911273e
LYChubby <f4uzi345@gmail.com>
2023-05-31 17:28:08 +0700

    Membuat Class Orang

---
+++ Multiwaris/Multiwaris.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		
+		cout << "Orang Dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "Orang Dihapus\n" << endl;
+	}
+};
