24888b1d5aed55a23b583262bb1688e214e73063
LYChubby <f4uzi345@gmail.com>
2023-05-31 17:35:33 +0700

    Membuat Class Pelajar

--- Multiwaris/Multiwaris.cpp
+++ Multiwaris/Multiwaris.cpp
@@ -29,3 +29,22 @@ public:
 		cout << "Manusia Dihapus\n" << endl;
 	}
 };
+
+class pelajar : private manusia, public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pJenisKelamin, string pSekolah) :
+		orang(pNama),
+		manusia(pJenisKelamin),
+		sekolah(pSekolah) {
+
+		cout << "Pelajar Dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "Pelajar Dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "Halo, Nama Saya " + nama + " Dengan Jenis kelamin " + jenisKelamin + " Dari Sekolah " + sekolah + "\n\n";
+	}
+};
